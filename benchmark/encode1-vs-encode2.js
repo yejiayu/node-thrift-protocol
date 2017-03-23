d052f3e6bb66a179e3d6783ee50fe52ac5f470d5
叶嘉雨 <jiayu.ye@ele.me>
2017-03-24 03:33:36 +0800

    Chore benchmark

--- benchmark/encode1-vs-encode2.js
+++ benchmark/encode1-vs-encode2.js
@@ -1,6 +1,7 @@
 const encode1 = require('./shim/encode1');
 const encode2 = require('./shim/encode2');
 const encode3 = require('./shim/encode3');
+const encode4 = require('./shim/encode4');
 
 const pingMessage = {
   name: 'ping',
@@ -26,6 +27,15 @@ const large = {
   }
 };
 
+const bigStr = {
+  name: Array.from({ length: 10000 }).reduce((p, c) => p += 'ping', 'ping'),
+  id: 10,
+  type: 'REPLY',
+  fields: [
+    { id: 0, type: 'BOOL', value: true }
+  ]
+};
+
 suite('encode', () => {
   bench('ping1', () => {
     encode1(pingMessage);
@@ -39,6 +49,10 @@ suite('encode', () => {
     encode3(pingMessage);
   });
 
+  bench('ping4', () => {
+    encode4(pingMessage);
+  });
+
   bench('large1', () => {
     encode1(large);
   });
@@ -49,5 +63,25 @@ suite('encode', () => {
 
   bench('large3', () => {
     encode3(large);
+  });
+
+  bench('large4', () => {
+    encode4(large);
+  });
+
+  bench('bigStr', () => {
+    encode1(bigStr);
+  });
+
+  bench('bigStr', () => {
+    encode2(bigStr);
+  });
+
+  bench('bigStr', () => {
+    encode3(bigStr);
+  });
+
+  bench('bigStr', () => {
+    encode4(bigStr);
   });
 });
